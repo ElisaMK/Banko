aa4fd9800f17e0c9a98d7c90f87553f5e8677dbd
Elisa <elisa_kemper@outlook.dk>
2017-11-06 12:42:26 +0100

    Cleaned some folders

---
+++ Banko1/txt filer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1{
+	public Class1(){
+
+        Random rnd = new Random();
+        int Value = rnd.Next(1, 10);
+
+        Console.WriteLine(Value);
+        Console.ReadLine();
+    }
+}
